6ff8925ca728536e9b3dd867666f289acd5e5f23
FedericoBusero <35894905+FedericoBusero@users.noreply.github.com>
2021-11-15 21:13:22 +0100

    dikkere slider
    https://github.com/FedericoBusero/Wifi-Hovercraft-Browser/issues/7

--- ESP8266_Hover_Browser/hovercontrol_html.h
+++ ESP8266_Hover_Browser/hovercontrol_html.h
@@ -45,9 +45,9 @@ const char index_html[] PROGMEM = R"=====(
 .slider-color {
   -webkit-appearance: none;
   width: 100%;
-  height: 15px;
+  height: 20px;
   margin-top: 10px;
-  margin-bottom: 10px;
+  margin-bottom: 15px;
   border-radius: 5px;
   background: #d3d3d3;
   outline: none;
@@ -61,15 +61,15 @@ const char index_html[] PROGMEM = R"=====(
 .slider-color::-webkit-slider-thumb {
   -webkit-appearance: none;
   appearance: none;
-  width: 25px;
-  height: 25px;
+  width: 40px;
+  height: 40px;
   border-radius: 50%;
   background: #4CAF50;
   cursor: pointer;
 }
 .slider-color::-moz-range-thumb {
-  width: 25px;
-  height: 25px;
+  width: 40px;
+  height: 40px;
   border: 0;
   border-radius: 50%;
   background: #4CAF50;
