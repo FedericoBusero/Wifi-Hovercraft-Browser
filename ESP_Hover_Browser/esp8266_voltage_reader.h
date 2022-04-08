f3aae1988b8399320731b54cdc5eb01fe3b6a048
FedericoBusero <35894905+FedericoBusero@users.noreply.github.com>
2022-04-08 20:21:01 +0200

    Delete esp8266_voltage_reader.h

--- ESP_Hover_Browser/esp8266_voltage_reader.h
+++
@@ -1,7 +0,0 @@
-// Niet verplaatsen naar .ino file want dit veroorzaakt preprocessor problemen
-
-// Nodig voor het inlezen van het voltage met ESP.getVcc
-ADC_MODE(ADC_VCC); 
-
-// Pas de voltagefactor aan, dat is bij elke chip hetzelfde. Calibreer bv. met USB stroom die 3.3V op de chip moet geven
-#define VOLTAGE_FACTOR 910.0f 
