8595e4964babfc0f1528f87d9a7b471a87d0e8d8
FedericoBusero <35894905+FedericoBusero@users.noreply.github.com>
2023-04-02 16:01:10 +0200

    DOCTYPE html (#70)

--- ESP_Hover_Browser/hovercontrol_html.h
+++ ESP_Hover_Browser/hovercontrol_html.h
@@ -1,4 +1,5 @@
 const char index_html[] PROGMEM = R"=====(
+<!DOCTYPE html>
 <html>
 <head>
 <meta name='viewport'         content='width=device-width,         initial-scale=1.0,         user-scalable=no' />
