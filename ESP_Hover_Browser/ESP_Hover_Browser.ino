c8e3badb9a079ae32005cd8d99f4651e08651434
FedericoBusero <35894905+FedericoBusero@users.noreply.github.com>
2023-09-20 22:09:45 +0200

    updatestatusbar gyro

--- ESP_Hover_Browser/ESP_Hover_Browser.ino
+++ ESP_Hover_Browser/ESP_Hover_Browser.ino
@@ -109,7 +109,7 @@ WebsocketsClient sclient;
 // timeoutes
 #define TIMEOUT_MS_MOTORS 2500L // Timeout om motoren uit veiligheid stil te leggen, na x milliseconden niks te hebben ontvangen
 #define TIMEOUT_MS_LED 1L        // Aantal milliseconden dat LED blijft branden na het ontvangen van een boodschap
-#define TIMEOUT_MS_VOLTAGE 10000L // Aantal milliseconden tussen update voltage
+#define TIMEOUT_MS_VOLTAGE 1000L // Aantal milliseconden tussen update voltage
 
 unsigned long last_activity_message;
 
@@ -540,6 +540,7 @@ void updatestatusbar()
 
       if (gyroBeschikbaar)
       {
+        sensor.read();
         snprintf(statusstr, sizeof(statusstr), "%4.2f V gz:%4.2f", voltage, sensor.getGyroZ());
       } else
       {
