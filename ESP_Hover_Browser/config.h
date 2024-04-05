ecddd8b170eb79aabfc1eb8a8860c62b849e93c2
FedericoBusero <35894905+FedericoBusero@users.noreply.github.com>
2024-04-05 20:32:25 +0200

    Update config.h

--- ESP_Hover_Browser/config.h
+++ ESP_Hover_Browser/config.h
@@ -7,7 +7,10 @@
 // #define ENV_HOVER_ESP8266_ESP01_LEDPIN1_V0
 // #define ENV_HOVER_ESP8266_ESP01_LEDPIN2_V0
 // #define ENV_HOVER_ESP8266_LOLIND1MINILITE
+// #define ENV_HOVER_ESP8266_NODEMCU
 
+// Als de defines in platformio.ini gedefinieerd zijn:
+// #define ENV_USER_DEFINED
 
 /*
 Als je een ander board wenst te gebruiken, zijn volgende defines nodig:
@@ -72,6 +75,24 @@ Op ESP8266-chips wordt het voltage gemeten, voeg volgende defines toe
 #define LED_BRIGHTNESS_ON  LOW
 #define LED_BRIGHTNESS_OFF HIGH
 
+#elif defined (ENV_HOVER_ESP8266_NODEMCU)
+
+#define DEBUG_SERIAL Serial
+
+#define PIN_SERVO          D2 // D2 = GPIO4  op NodeMCU & Wemos D1 mini
+#define PIN_MOTOR          D8 // D8 = GPIO15 op NodeMCU & Wemos D1 mini
+// De ingebouwde LED zit meestal op GPIO2 of GPIO16
+#define PIN_LEDCONNECTIE    D0 // D0=GPIO16
+
+// Pas de voltagefactor aan, dat is bij elke chip verschillend. Calibreer bv. met USB stroom die 3.3V op de chip moet geven
+#define VOLTAGE_FACTOR 910.0f 
+#define VOLTAGE_THRESHOLD 2.4 // onder dit voltage valt de chip uit om de batterij te beschermen
+
+#define LED_BRIGHTNESS_ON  LOW
+#define LED_BRIGHTNESS_OFF HIGH
+
+#elif defined ENV_USER_DEFINED
+// defines staan buiten de code
 #else
 // Geen ENV_XX geselecteerd
 #error "Defineer één van bovenstaande defines"
