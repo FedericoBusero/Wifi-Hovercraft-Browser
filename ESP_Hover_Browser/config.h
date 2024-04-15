059a358bc0fbfaeea39c0761da41ac4206276845
FedericoBusero <35894905+FedericoBusero@users.noreply.github.com>
2024-04-15 21:14:23 +0200

    wifi settings verhuizen naar config.h

--- ESP_Hover_Browser/config.h
+++ ESP_Hover_Browser/config.h
@@ -13,7 +13,7 @@
 // #define ENV_USER_DEFINED
 
 /*
-Als je een ander board wenst te gebruiken, zijn volgende defines nodig:
+Als je een ander board wenst te definiëren, zijn volgende defines nodig:
 * Als je seriële output wenst (en de RX/TX pinnen zijn niet in gebruik voor andere doelen):
 #define DEBUG_SERIAL Serial
 
@@ -26,12 +26,23 @@ Daarnaast zijn volgende defines verplicht (maar kunnen omgewisseld worden)
 #define LED_BRIGHTNESS_ON  HIGH
 #define LED_BRIGHTNESS_OFF LOW
 
-Op ESP8266-chips wordt het voltage gemeten, voeg volgende defines toe
-// Pas de voltagefactor aan, dat is bij elke chip verschillend. Calibreer bv. met USB stroom die 3.3V op de chip moet geven
+Op ESP8266-chips wordt het voltage gemeten, voeg volgende define toe. Pas de voltagefactor aan, dat is bij elke chip verschillend. 
+Calibreer bv. met USB stroom die 3.3V op de chip moet geven
+
 #define VOLTAGE_FACTOR 1060.0f 
-#define VOLTAGE_THRESHOLD 2.4 // onder dit voltage valt de chip uit om de batterij te beschermen
 
 */
+
+#ifndef ENV_USER_DEFINED
+
+// Volgende defines zijn op alle borden van toepassing
+#define WIFI_SOFTAP_SSID_PREFIX "hover-"
+#define WIFI_SOFTAP_PASSWORD "12345678"
+#define WIFI_SOFTAP_CHANNEL 1 // 1-13
+
+#define VOLTAGE_THRESHOLD 2.7 // onder dit voltage valt de chip uit om de batterij te beschermen
+
+#endif
 
 #if defined(ENV_HOVERSERVO_ESP8266_ESP01_LEDPIN1_V0)
 
@@ -41,7 +52,6 @@ Op ESP8266-chips wordt het voltage gemeten, voeg volgende defines toe
 
 // Pas de voltagefactor aan, dat is bij elke chip verschillend. Calibreer bv. met USB stroom die 3.3V op de chip moet geven
 #define VOLTAGE_FACTOR 1060.0f 
-#define VOLTAGE_THRESHOLD 2.4 // onder dit voltage valt de chip uit om de batterij te beschermen
 
 #define LED_BRIGHTNESS_ON  LOW
 #define LED_BRIGHTNESS_OFF HIGH
@@ -54,7 +64,6 @@ Op ESP8266-chips wordt het voltage gemeten, voeg volgende defines toe
 
 // Pas de voltagefactor aan, dat is bij elke chip verschillend. Calibreer bv. met USB stroom die 3.3V op de chip moet geven
 #define VOLTAGE_FACTOR 1060.0f 
-#define VOLTAGE_THRESHOLD 2.4 // onder dit voltage valt de chip uit om de batterij te beschermen
 
 #define LED_BRIGHTNESS_ON  LOW
 #define LED_BRIGHTNESS_OFF HIGH
@@ -70,7 +79,6 @@ Op ESP8266-chips wordt het voltage gemeten, voeg volgende defines toe
 
 // Pas de voltagefactor aan, dat is bij elke chip verschillend. Calibreer bv. met USB stroom die 3.3V op de chip moet geven
 #define VOLTAGE_FACTOR 910.0f 
-#define VOLTAGE_THRESHOLD 2.4 // onder dit voltage valt de chip uit om de batterij te beschermen
 
 #define LED_BRIGHTNESS_ON  LOW
 #define LED_BRIGHTNESS_OFF HIGH
@@ -86,7 +94,6 @@ Op ESP8266-chips wordt het voltage gemeten, voeg volgende defines toe
 
 // Pas de voltagefactor aan, dat is bij elke chip verschillend. Calibreer bv. met USB stroom die 3.3V op de chip moet geven
 #define VOLTAGE_FACTOR 910.0f 
-#define VOLTAGE_THRESHOLD 2.4 // onder dit voltage valt de chip uit om de batterij te beschermen
 
 #define LED_BRIGHTNESS_ON  LOW
 #define LED_BRIGHTNESS_OFF HIGH
