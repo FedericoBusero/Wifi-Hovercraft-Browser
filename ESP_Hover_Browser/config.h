17b58d773ca2ed1b23a74ec7639b76cc374e36ca
FedericoBusero <35894905+FedericoBusero@users.noreply.github.com>
2025-12-07 16:22:06 +0100

    config.h: ENV_HOVERSERVOGYRO_ESP32C3_WROOM_V1

--- ESP_Hover_Browser/config.h
+++ ESP_Hover_Browser/config.h
@@ -11,6 +11,7 @@
 
 // #define ENV_HOVERSERVOGYRO_ESP8266_ESP01_LEDPIN2_V0
 // #define ENV_HOVERSERVOGYRO_ESP32C3_SUPERMINI_WS2812FX_V0
+// #define ENV_HOVERSERVOGYRO_ESP32C3_WROOM_V1
 
 // Als de defines in platformio.ini gedefinieerd zijn:
 // #define ENV_USER_DEFINED
@@ -22,7 +23,7 @@ Als je een ander board wenst te definiëren, zijn volgende defines nodig:
 - FASTIMU_TYPE
 - IMU_I2C_ADDRESS
 - GYRO_REGELING_P
-- GYRO_REGELING_MAX_DRAAI
+- GYRO_REGELING_MAX_DRAAI (optioneel default 1.0)
 - GYRO_REGELING_BIAS
 - GYRO_DIRECTION : GYRO_DIRECTION_X, GYRO_DIRECTION_Y of GYRO_DIRECTION_Z
 - GYRO_LPF_TF
@@ -220,6 +221,51 @@ enum
 
 #define VOLTAGE_FACTOR 850.0f
 
+#elif defined(ENV_HOVERSERVOGYRO_ESP32C3_WROOM_V1) // LSM6DS3, met achteruit (Marinedagen 2025)
+
+#define USE_CONFIG_HOVERSERVO
+
+// #define DEBUG_SERIAL Serial
+
+#define USE_FASTIMU
+#define FASTIMU_TYPE LSM6DS3
+#define IMU_I2C_ADDRESS 0x6B
+#define GYRO_DIRECTION GYRO_DIRECTION_X
+//#define GYRO_FLIP
+#define GYRO_REGELING_P         4.0
+#define GYRO_REGELING_MAX_DRAAI 1.0
+#define GYRO_REGELING_BIAS      1.0
+// #define SERVO_ANTI_BIBBER       3.0
+#define GYRO_LPF_TF             0.100 // Tf in seconds
+
+#define PIN_SERVO          10
+#define PIN_MOTOR          7
+#define PIN_MOTOR2         4
+
+#define PIN_LEDCONNECTIE   8
+#define PIN_LED_DUALUSE
+#define PIN_SDA            2          
+#define PIN_SCL            6
+
+#define PIN_BATMONITOR     1
+
+#define MOTORZ_TIME_UP 2000 // ms om motor naar vol vermogen te brengen
+
+#define LED_BRIGHTNESS_ON  LOW
+#define LED_BRIGHTNESS_OFF HIGH
+
+//#define USE_WS2812FX
+#define PIN_WS2812FX       8
+#define WS2812FX_NUMLEDS    6
+#define WS2812FX_RGB_ORDER  NEO_BGR //voor "fairy" type
+#define WS2812FX_BRIGHTNESS 255 // 0 .. 255
+#define WS2812FX_SPEED 1000 // in ms
+#define WS2812FX_COLOR 0x007BFF // blauw, 0x007BFF geeft violet en blauw met 0xFF0000 op fairy type met NEO_GRB?
+#define WS2812FX_MODE FX_MODE_FADE // FX_MODE_BLINK, ... Volledige lijst op https://github.com/kitesurfer1404/WS2812FX/blob/master/src/modes_arduino.h
+
+#define VOLTAGE_FACTOR 850.0f
+
+
 #elif defined ENV_USER_DEFINED
 // defines staan buiten de code
 
@@ -236,4 +282,5 @@ enum
 
 #endif
 #endif
+
 
